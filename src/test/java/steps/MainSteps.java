086dd669e85f4cee49ed1505b283b2b4275f391e
Nikola Djordjevic <djordjevic.nikola.it@gmail.com>
2023-05-14 12:48:37 +0200

    Initial Commit of the Cucumber Demo Project

---
+++ src/test/java/steps/MainSteps.java
@@ -0,0 +1,93 @@
+package steps;
+
+import excel.ExelReader;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Reporter;
+import pages.SauceDemoLoginPage;
+import tests.BaseTest;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class MainSteps extends BaseTest {
+
+    String browser = Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest().getParameter("browser");
+    String quit = Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest().getParameter("quit");
+    String wait = Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest().getParameter("wait");
+    Map<String, String>data;
+
+    @Before
+    public void setup() throws Exception {
+        init(browser,wait);
+    }
+
+    @After
+    public void tearDown(){
+        if(quit.equalsIgnoreCase("Yes")) {
+            quit();
+        }
+    }
+
+    @Given("I load test data from {string} {string} {string}")
+    public void iLoadTestDataFrom(String file, String sheet, String row) throws IOException {
+        data=new ExelReader().getRowData(file,sheet,Integer.parseInt(row));
+    }
+
+    @Given("I load test data from {string} {string} for {string}")
+    public void iLoadTestDataFromFor(String file, String sheet, String tc_id) throws IOException {
+        data=new ExelReader().getRowDataByID(file,sheet,tc_id);
+    }
+    @Given("I am on the sauce demo login page")
+    public void iAmOnTheSauceDemoLoginPage(){
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @When("I enter my username {string}")
+    public void iEnterMyUsername(String username) throws Exception {
+        new SauceDemoLoginPage(driver).enterUsername(username);
+    }
+
+    @When("I enter my username")
+    public void iEnterMyUsername() throws Exception {
+        new SauceDemoLoginPage(driver).enterUsername(data.get("username"));
+    }
+
+    @And("I enter my password {string}")
+    public void iEnterMyPassword(String password) throws Exception {
+        new SauceDemoLoginPage(driver).enterPassword(password);
+    }
+
+    @And("I enter my password")
+    public void iEnterMyPassword() throws Exception {
+        new SauceDemoLoginPage(driver).enterPassword(data.get("password"));
+    }
+
+    @And("I click on the login button")
+    public void iClickOnTheLoginButton() throws Exception {
+        new SauceDemoLoginPage(driver).clickLogin();
+    }
+
+    //Inventory page
+    @Then("I should be logged in")
+    public void iShouldBeLoggedIn() {
+        new SauceDemoLoginPage(driver).menuShouldBeVisible();
+    }
+
+    @And("I should be able to see products")
+    public void iShouldBeAbleToSeeProducts() {
+        new SauceDemoLoginPage(driver).titleShouldBeVisible();
+    }
+    //Inventory page
+    @Then("I should get an error message {string}")
+    public void iShouldGetAnErrorMessage(String errorMessage) {
+        new SauceDemoLoginPage(driver).verifyErrorMessage(errorMessage);
+    }
+
+
+
+}
